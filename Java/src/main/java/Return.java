99cb654d7c83a39d7d9f0acc048e325227040177
Rahul Mandal <rahul.mandal@mphasis.com>
2025-01-27 10:46:23 +0530

    first commit

--- Java/src/main/java/Return.java
+++ Java/src/main/java/Return.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Return {
+import java.util.Scanner;
+
+public class Return {
+    public static void main(String[] args) {
+
+        greet();
+       int first = readNumber();
+       int second = readNumber();
+
+        int sum = first + second;
+        System.out.println("Sum of the number is " + sum);
+
+
+    }
+
+    public static void greet(){
+        System.out.println("Welcome to Calculator");
+    }
+
+    public static int readNumber() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the number");
+//        int number = input.nextInt();
+//        return number;   // alternate
+        return input.nextInt();
+    }
 }
