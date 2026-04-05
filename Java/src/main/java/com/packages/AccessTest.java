c098656d3a7de2ed4db74192b0fa18585cae6dd2
SE-rahulmandal18 <110009121+SE-rahulmandal18@users.noreply.github.com>
2026-04-05 11:46:45 +0530

    Update AccessTest.java

--- Java/src/main/java/com/packages/AccessTest.java
+++ Java/src/main/java/com/packages/AccessTest.java
@@ -5,7 +5,9 @@ public class AccessTest {
         Car car = new Car();
         car.color="Red";
         car.model="Swift";
-        car.costOfPurchase=733;
+        car.fuelLevel=5.0 // cannot be access as it is private in car class    
+        car.costOfPurchase=733;  // can be access as it has default access in car class,meaning AccessTest and Car class are in same package
+        
         System.out.println(car);
 
         Car newCar = new Car("Black", "BMW",
