d936ef1d624313a59fa846a3829a553a27a7378d
SE-rahulmandal18 <110009121+SE-rahulmandal18@users.noreply.github.com>
2026-04-05 11:53:29 +0530

    updated

--- Java/src/main/java/com/knowledgegate/DefaultTest.java
+++ Java/src/main/java/com/knowledgegate/DefaultTest.java
@@ -6,7 +6,9 @@ public class DefaultTest {
     public static void main(String[] args) {
         Car car = new Car("YELLOW", "Dezire",
                 5, 3000);
-//        car.costOfPurchase=8;
+//        car.costOfPurchase=8;  // cannnot be access as the property has default access-within the same package
+
+        Default default = new Default() // cannot create object of the default class , as it has default access
 
     }
 }
