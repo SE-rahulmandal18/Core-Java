228d0ceb0f35c4c467203bb9137bdedbd717c9de
SE-rahulmandal18 <110009121+SE-rahulmandal18@users.noreply.github.com>
2026-04-09 11:34:27 +0530

    Create wrapper.java

---
+++ JavaYT/src/in/kgcoding/varargs/wrapper.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class WrapperTest {
+    public static void main(String[] args) {
+        
+        Integer first = 55; // Auto boxing
+        System.out.println(first);
+        
+        Integer second = first;
+         System.out.println(second);  // Unboxing
+
+    }
+    
+}
