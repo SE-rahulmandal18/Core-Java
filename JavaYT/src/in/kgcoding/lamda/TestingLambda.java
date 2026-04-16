a28492b3f0b0826f0481c906d65e10e228ee05b6
SE-rahulmandal18 <110009121+SE-rahulmandal18@users.noreply.github.com>
2026-04-16 09:43:14 +0530

    lambda

--- JavaYT/src/in/kgcoding/lamda/TestingLambda.java
+++ JavaYT/src/in/kgcoding/lamda/TestingLambda.java
@@ -2,9 +2,20 @@ package in.kgcoding.lamda;
 
 public class TestingLambda {
     public static void main(String[] args) {
-        TestingLambda test = new TestingLambda();
-        int sum = test.sum(6,7);
-        test.printString("This is the best course");
+
+        Thread t1 = new Thread(new Task()); // here we need to pass the object of runnable 
+        // so to simply this we have lambda expression as below 
+
+         Thread t1 = new Thread(() -> {
+        system.out.println("Hello");
+            }); 
+      
+
+        
+        
+        // TestingLambda test = new TestingLambda();
+        // int sum = test.sum(6,7);
+        // test.printString("This is the best course");
 
         // toPrint -> System.out.println(toPrint);
         // (a, b) -> a + b;
@@ -14,11 +25,18 @@ public class TestingLambda {
         //}
     }
 
-    public void printString(String toPrint){
-        System.out.println(toPrint);
-    }
-    public int sum(int a, int b){
-        int sum = a+b;
-        return sum;
+    // public void printString(String toPrint){
+    //     System.out.println(toPrint);
+    // }
+    // public int sum(int a, int b){
+    //     int sum = a+b;
+    //     return sum;
     }
 }
+
+class Task implements Runnable {
+
+    @Override
+    public void run() {
+        system.out.println("Hello");
+    }
